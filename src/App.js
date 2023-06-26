7365d760493ce080a437ef109f11a12865a75487
divya <divya@qpicloud.tech>
2023-06-26 21:09:40 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { Spin } from "antd";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const fetchQuote = async () => {
+    setLoading(true);
+    // const response =
+    await axios
+      .get(`https://api.api-ninjas.com/v1/quotes?category=love`, {
+        headers: {
+          "X-Api-Key": "v/QIKNv4TO6FdQZyZfG9og==NrDWBOALPHpj7cgy",
+        },
+        method: "GET",
+      })
+      .then((response) => {
+        console.log(response.data[0].quote);
+        setQuote(response.data[0].quote);
+        setLoading(false);
+      });
+    // console.log(response.data[0].quote);
+    // setQuote(response.data[0].quote);
+    // setLoading(false);
+  };
+
+  const handleClick = () => {
+    fetchQuote();
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <div className="content-box">
+          <p className="quote-title">~ quote of the day ~</p>
+          <h3 className="quote-content"> {loading ? <Spin /> : quote} </h3>
+          <button onClick={handleClick}>get random</button>
+        </div>
+      </div>
     </div>
   );
 }
